768ecc52450fdc598ec2f4d34f9f15797adc6863
Erkraker <118482710+Erkraker@users.noreply.github.com>
2026-06-12 20:26:06 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,16 @@
-const CACHE_NAME='controle-insertos-grau-v1';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>self.clients.claim());
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = 'controle-insertos-v4-grau-atual-20260612-01';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
